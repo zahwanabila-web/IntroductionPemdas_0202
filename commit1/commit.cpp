fa26878af5c2d0f52b2126586589e43d183f688f
ZahwaNabila <zahwanabila434@gmail.com>
2026-03-09 20:19:36 +0700

    commit8

--- commit1/commit.cpp
+++ commit1/commit.cpp
@@ -12,3 +12,22 @@ string status_lulus(float r){
         return "Gagal";
 }
 
+int main(){
+    float niLBI,niLMT,rerata;
+    string status;
+
+    cout <<"Masukkan nilai Bahasa Inggris = ";
+    cin >> niLBI;
+    cout <<"Masukkan nilai matematika = ";
+    cin >> niLMT;
+
+    //rerata = (niLBI+niLMT)/2;
+    //if (rerata >= 69)
+    //    status = "lolos";
+    //else
+      // status = "gagal";
+      rerata = Rata_rata(niLBI,niLMT);
+      //status = Status_Lulus(rerata);
+
+      cout << "Status kelulusan = "<<Status_lulus(rerata)<<" ,dengan nilai rerata ="<<rerata<<endl; 
+}
